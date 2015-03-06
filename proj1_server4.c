19737965472c21497ef0627007a890bc835df614
jessica <jessicak@berkeley.edu>
2015-03-06 07:52:11 -0800

    server4 fix

--- proj1_server4.c
+++ proj1_server4.c
@@ -22,7 +22,9 @@
  	double packet_delay;
  	struct sockaddr_in server, client;
  	char buf[7];
- 	char all_buf[PACKET_SIZE*10] = "1111111111222222222233333333334444444444555555555566666666667777777777888888888899999999990000000000"; //used for connectless recv to establish connection
+    char all_buf[10] = "0123456789";
+ //	char all_buf[PACKET_SIZE*10] = "1111111111222222222233333333334444444444555555555566666666667777777777888888888899999999990000000000"; //used for connectless recv to establish connection
+ 	//char all_buf[PACKET_SIZE*10] = "1111111111222222222233333333334444444444555555555566666666667777777777888888888899999999990000000000"; //used for connectless recv to establish connection
  	char buffer[PACKET_SIZE];
 
  	if(argc != 2 || !strcmp(argv[1], "-h"))
@@ -80,13 +82,13 @@
 		printf("\n[server4]\tGot a new client!\n");
 
 		while (1)) { // send 10 packets total
-			strncpy(buffer, all_buf + (p*10), 10);
-			buffer[10]='\0';
-			puts(buffer);
+			//strncpy(buffer, all_buf + (p*10), 10);
+			//buffer[10]='\0';
+			puts(all_buffer);
 			//set packet delay to random double between 1 and 10
 			packet_delay = (rand() / (double)(RAND_MAX/10)) ;
 
-			if (sendto(listen_fd, buffer, PACKET_SIZE, 0, (struct sockaddr *) &client, client_len) < 0) {
+			if (sendto(listen_fd, all_buffer, PACKET_SIZE, 0, (struct sockaddr *) &client, client_len) < 0) {
 				printf("[server]\tError: Failed sending packet.\n");
 				perror("sendto");
 			}
